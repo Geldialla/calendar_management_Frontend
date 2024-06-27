20ab206fdbf12309019ee14390759cc46d612b41
Geldi Alla <geldiallaal@gmail.com>
2024-06-27 16:54:01 +0200

    weather update

--- src/app/admin/hierarchyy/add-chart/add-chart.component.ts
+++ src/app/admin/hierarchyy/add-chart/add-chart.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError } from 'rxjs/operators';
